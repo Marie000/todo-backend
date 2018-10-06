c49927c841d7b1229c58eab46ade5c0e0f0432d7
Marie Pelletier <marie.pelletier@gmail.com>
2018-10-06 14:08:58 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+app.get('/', (req,res) => {
+  res.send('hello server!')
+
+});
+
+mongoose.connect(process.env.MONGODB_URI, () => {
+  app.listen(3000, function() {
+    console.log('server listening on port 3000');
+  });
+})
